936001682f31bc2f3a15caf250b673af8880f58c
yanan5 <obrnarayanan@gmail.com>
2020-05-22 18:20:54 +0530

    added colors and customized axes

--- barChart.js
+++ barChart.js
@@ -1,45 +1,82 @@
-import * as d3 from 'd3';
+import * as d3 from "d3";
 
-const {select, scaleLinear, max, scaleBand, axisLeft, csv, axisBottom} = d3
+const {
+  select,
+  scaleLinear,
+  max,
+  scaleBand,
+  axisLeft,
+  csv,
+  axisBottom,
+  format,
+} = d3;
 
-const svg = select('svg');
+const svg = select("svg");
 
-const width = +svg.attr('width');
-const height = +svg.attr('height');
+const width = +svg.attr("width");
+const height = +svg.attr("height");
 
-const render = data => {
-  const xValue = d => d.population;
-  const yValue = d => d.country;
-  const margin = { top: 20, right: 20, bottom: 20, left: 70}
+const render = (data) => {
+  const xValue = (d) => d.population;
+  const yValue = (d) => d.country;
+  const margin = { top: 30, right: 20, bottom: 50, left: 130 };
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
-  
+
   const xScale = scaleLinear()
     .domain([0, max(data, xValue)])
-    .range([0, innerWidth])
-  
+    .range([0, innerWidth]);
+
   const yScale = scaleBand()
     .domain(data.map(yValue))
     .range([0, innerHeight])
-    .padding(0.1)
+    .padding(0.1);
+
+  const xAxisTickFormat = (number) => format(".3s")(number).replace("G", "B");
 
   const yAxis = axisLeft(yScale);
-  const xAxis = axisBottom(xScale);
-  const g = svg.append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`);
+  const xAxis = axisBottom(xScale)
+    .tickFormat(xAxisTickFormat)
+    .tickSize(-innerHeight);
 
-  g.append('g').call(yAxis)
-  g.append('g').call(xAxis)    
-  .attr('transform', `translate(0, ${innerHeight})`);
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  g.selectAll('rect').data(data)
-    .enter().append('rect')
-      .attr('y', d => yScale(yValue(d)))
-      .attr('width', d => xScale(xValue(d)))
-      .attr('height', yScale.bandwidth())
-}
-csv('./data/population.csv').then(data => {
-  data.forEach(d => d.population = d.population * 1000)
+  g.append("g")
+    .call(yAxis)
+    .selectAll(".domain, .tick line")
+    .remove();
+
+  const xAxisG = g.append("g")
+    .call(xAxis)
+    .attr("transform", `translate(0, ${innerHeight})`)
+    
+    xAxisG.select(".domain")
+      .remove()
+  
+    xAxisG.append('text')
+      .text('Population')
+      .attr('class', 'axis-label')
+      .attr('fill','#000')
+      .attr('x', innerWidth/2)
+      .attr('y', margin.bottom - 5)
+
+  g.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("y", (d) => yScale(yValue(d)))
+    .attr("width", (d) => xScale(xValue(d)))
+    .attr("height", yScale.bandwidth());
+
+  g.append('text')
+    .text('Top 10 most populous countries')    
+      .attr('class', 'title')
+      .attr('y', -5);
+};
+csv("./data/population.csv").then((data) => {
+  data.forEach((d) => (d.population = d.population * 1000));
   console.log(data);
-  render(data)
-})
+  render(data);
+});
--- d3.js
+++ d3.js
@@ -18575,46 +18575,83 @@
     zoomIdentity: identity$9
   });
 
-  const {select: select$1, scaleLinear, max: max$3, scaleBand, axisLeft: axisLeft$1, csv: csv$2, axisBottom: axisBottom$1} = d3;
+  const {
+    select: select$1,
+    scaleLinear,
+    max: max$3,
+    scaleBand,
+    axisLeft: axisLeft$1,
+    csv: csv$2,
+    axisBottom: axisBottom$1,
+    format: format$1,
+  } = d3;
 
-  const svg$1 = select$1('svg');
+  const svg$1 = select$1("svg");
 
-  const width = +svg$1.attr('width');
-  const height = +svg$1.attr('height');
+  const width = +svg$1.attr("width");
+  const height = +svg$1.attr("height");
 
-  const render = data => {
-    const xValue = d => d.population;
-    const yValue = d => d.country;
-    const margin = { top: 20, right: 20, bottom: 20, left: 70};
+  const render = (data) => {
+    const xValue = (d) => d.population;
+    const yValue = (d) => d.country;
+    const margin = { top: 30, right: 20, bottom: 50, left: 130 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
-    
+
     const xScale = scaleLinear()
       .domain([0, max$3(data, xValue)])
       .range([0, innerWidth]);
-    
+
     const yScale = scaleBand()
       .domain(data.map(yValue))
       .range([0, innerHeight])
       .padding(0.1);
 
+    const xAxisTickFormat = (number) => format$1(".3s")(number).replace("G", "B");
+
     const yAxis = axisLeft$1(yScale);
-    const xAxis = axisBottom$1(xScale);
-    const g = svg$1.append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
+    const xAxis = axisBottom$1(xScale)
+      .tickFormat(xAxisTickFormat)
+      .tickSize(-innerHeight);
 
-    g.append('g').call(yAxis);
-    g.append('g').call(xAxis)    
-    .attr('transform', `translate(0, ${innerHeight})`);
+    const g = svg$1
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    g.selectAll('rect').data(data)
-      .enter().append('rect')
-        .attr('y', d => yScale(yValue(d)))
-        .attr('width', d => xScale(xValue(d)))
-        .attr('height', yScale.bandwidth());
+    g.append("g")
+      .call(yAxis)
+      .selectAll(".domain, .tick line")
+      .remove();
+
+    const xAxisG = g.append("g")
+      .call(xAxis)
+      .attr("transform", `translate(0, ${innerHeight})`);
+      
+      xAxisG.select(".domain")
+        .remove();
+    
+      xAxisG.append('text')
+        .text('Population')
+        .attr('class', 'axis-label')
+        .attr('fill','#000')
+        .attr('x', innerWidth/2)
+        .attr('y', margin.bottom - 5);
+
+    g.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("y", (d) => yScale(yValue(d)))
+      .attr("width", (d) => xScale(xValue(d)))
+      .attr("height", yScale.bandwidth());
+
+    g.append('text')
+      .text('Top 10 most populous countries')    
+        .attr('class', 'title')
+        .attr('y', -5);
   };
-  csv$2('./data/population.csv').then(data => {
-    data.forEach(d => d.population = d.population * 1000);
+  csv$2("./data/population.csv").then((data) => {
+    data.forEach((d) => (d.population = d.population * 1000));
     console.log(data);
     render(data);
   });
