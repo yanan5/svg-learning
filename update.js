cac84b890b4afb79b2fed2c3781ae8f6f9cb4984
yanan5 <obrnarayanan@gmail.com>
2020-06-03 15:52:04 +0530

    added animation for enter update and exit

--- update.js
+++ update.js
@@ -5,13 +5,13 @@ const SVG = select("svg");
 const WIDTH = +SVG.attr("width");
 const HEIGHT = +SVG.attr("height");
 const colorScale = scaleOrdinal()
-  .domain(['apple', 'lemon'])
-  .range(['#BD2D28', '#F2DA57'])
-const radiusScale = scaleOrdinal()
-  .domain(['apple', 'lemon'])
-  .range([50, 30])
+  .domain(["apple", "lemon"])
+  .range(["#BD2D28", "#F2DA57"]);
+const radiusScale = scaleOrdinal().domain(["apple", "lemon"]).range([50, 30]);
 const render = (selection, { data }) => {
-  const UPDATE_SELECTION = selection.selectAll("circle").data(data);
+  const UPDATE_SELECTION = selection
+    .selectAll("circle")
+    .data(data, (d) => d.id);
   const ENTER_SELECTION = UPDATE_SELECTION.enter();
   const EXIT_SELECTION = UPDATE_SELECTION.exit();
   // make an enter selection
@@ -19,16 +19,35 @@ const render = (selection, { data }) => {
     .append("circle")
       .attr("cx", (d, i) => i * 120 + 60)
       .attr("cy", 50)
+      .attr("r", 0)
     .merge(UPDATE_SELECTION)
-      .attr("fill", d => colorScale(d.type))
-      .attr("r", d => radiusScale(d.type));
+      .attr("fill", (d) => colorScale(d.type))
+    .transition().duration(1000)
+      .attr("r", (d) => radiusScale(d.type))
+      .attr("cx", (d, i) => i * 120 + 60)
 
-  EXIT_SELECTION
-    .remove();
+  EXIT_SELECTION.transition().duration(1000).attr("r", 0).remove();
+
+  // const UPDATE_TEXT = selection.selectAll("text").data(data, d => d.id);
+  // const ENTER_TEXT = UPDATE_TEXT.enter();
+  // const EXIT_TEXT = UPDATE_TEXT.exit();
+  //   // make an enter selection
+  //   ENTER_TEXT
+  //     .append("text")
+  //       .attr("x", (d, i) => i * 120 + 60)
+  //       .attr("y", 50)
+  //     .merge(UPDATE_TEXT)
+  //     .transition().duration(1000)
+  //       .attr("x", (d, i) => i * 120 + 60)
+  //       .text(d => d.type)
+
+  //   EXIT_TEXT
+  //     .transition().duration(1000)
+  //     .remove();
 };
-const makeFruit = (type) => ({ type });
+const makeFruit = (type) => ({ type, id: Math.random() });
 
-const fruits = range(5).map(() => makeFruit("apple"));
+let fruits = range(5).map(() => makeFruit("apple"));
 render(SVG, { data: fruits });
 setTimeout(() => {
   // remove on apple
@@ -38,6 +57,12 @@ setTimeout(() => {
 
 setTimeout(() => {
   // remove on apple
-  fruits[2].type = 'lemon';
+  fruits[2].type = "lemon";
   render(SVG, { data: fruits });
 }, 2000);
+
+setTimeout(() => {
+  // remove on apple
+  fruits = fruits.filter((d, i) => i !== 1);
+  render(SVG, { data: fruits });
+}, 3000);
