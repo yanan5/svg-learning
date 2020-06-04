fd6ab1e69cec9ec10ed8e1f3420b29702b6f41ed
yanan5 <obrnarayanan@gmail.com>
2020-06-04 20:48:44 +0530

    fix: added visual marks for click handlers

--- update.js
+++ update.js
@@ -3,13 +3,21 @@ const { select, range, scaleOrdinal } = d3;
 
 const SVG = select("svg");
 const WIDTH = +SVG.attr("width");
+const makeFruit = (type) => ({ type, id: Math.random() });
 const HEIGHT = +SVG.attr("height");
+let selected = null;
+
+let fruits = range(5).map(() => makeFruit("apple"));
 const colorScale = scaleOrdinal()
   .domain(["apple", "lemon"])
   .range(["#BD2D28", "#F2DA57"]);
 const radiusScale = scaleOrdinal()
   .domain(["apple", "lemon"])
   .range([50, 30]);
+const onClickHandler = d => {
+  selected = d.id;
+  render(SVG, {data: fruits})
+}
 const render = (selection, { data }) => {
   const BOWL = selection
     .selectAll('rect')
@@ -33,10 +41,15 @@ const render = (selection, { data }) => {
     .attr("transform", (d, i) => `translate(${i * 120 + 60}, 60)`)
   // make an enter selection
   g.append("circle")
-    .merge(UPDATE_GROUP.select("circle"))
-    .transition().delay(500)
+    .merge(UPDATE_GROUP.select("circle"))    
+    .on('click', onClickHandler)
+    .style('cursor', 'pointer')
+    .attr('stroke', d => d.id === selected ? '#000' : 'none')
+    .attr('stroke-width', 5)
+    .transition().delay(500)    
     .attr("fill", (d) => colorScale(d.type))
-    .attr("r", (d) => radiusScale(d.type));
+    
+    .attr("r", (d) => radiusScale(d.type))
 
   // make an enter selection
   g.append("text")
@@ -47,9 +60,7 @@ const render = (selection, { data }) => {
 
   EXIT_GROUP.transition().remove();
 };
-const makeFruit = (type) => ({ type, id: Math.random() });
 
-let fruits = range(5).map(() => makeFruit("apple"));
 render(SVG, { data: fruits });
 setTimeout(() => {
   // remove on apple
