b6ad7b6043a21c5d3cd1ec088801a6b0e25c5b09
yanan5 <obrnarayanan@gmail.com>
2020-06-10 19:31:40 +0530

    added map with basic zoom

--- map.js
+++ map.js
@@ -2,18 +2,34 @@ import * as d3 from "d3";
 import * as topojson from "topojson-client";
 
 const {feature} = topojson;
-const { select, json, geoPath, geoMercator, geoOrthographic,  } = d3;
+const { select, json, geoPath, zoom, tsv, geoNaturalEarth1 } = d3;
 
 const svg = select('svg');
-const projection = geoOrthographic();
+const projection = geoNaturalEarth1();
 const pathGenerator = geoPath().projection(projection)
 svg.append('path')
   .attr('class', 'sphere')
   .attr('d', pathGenerator({
     type: "Sphere"
   }))
-json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
-  .then(data => {
+
+svg.call(zoom().on('zoom' () => {
+  console.log('zoom')
+}))
+
+Promise.all([
+  tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv'),
+  json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
+])
+.then(([tsvData, topoJSONData]) => {  
+  const countryName = {};
+  tsvData.forEach(d => {
+    countryName[d.iso_n3] = d.name
+  });
+  renderMap(topoJSONData,countryName)
+})
+
+const renderMap = (data, countryLookUp) => {
     const countries = feature(data, data.objects.countries)
 
     const pathsUpdateSelection = svg
@@ -24,6 +40,8 @@ json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
 
     pathsEnterSelection
       .append('path')
-      .attr('class', 'countries')
-      .attr('d', pathGenerator)
-  })
+        .attr('class', 'country')
+        .attr('d', pathGenerator)
+      .append('title')
+        .text(d => countryLookUp[d.id])
+  }
